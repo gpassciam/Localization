f0bb4257636026b7e855ef31b3b3c1660c7f3d49
gpassciam <104811739+gpassciam@users.noreply.github.com>
2023-07-28 00:21:21 +0530

    Update gigya.js

--- gigya.js
+++ gigya.js
@@ -5,38 +5,10 @@ function ShowLoginScreen(lang)
             screenSet: ' Localization-China-RegistrationLogin',
             startScreen: 'gigya-login-screen',
             //customLang: customLangParams,
-            containerID: 'LoginScreen',
-            lang: lang
+            containerID: 'LoginScreen'
+            //lang: lang
             
         });
 
-    
-}
-function ShowForgotPassScreen()
-{
-    
-    gigya.accounts.showScreenSet({
-            screenSet: 'Localization-China-RegistrationLogin',
-            startScreen: 'gigya-forgot-password-screen',
-            //customLang: customLangParams,
-            containerID: 'ShowForgotPassScreen'
-            
-        });
-
-    
-    
-}
-function ShowResetPassScreen()
-{
-    
-    gigya.accounts.showScreenSet({
-            screenSet: 'coral-dev--RegistrationLogin',
-            startScreen: 'gigya-reset-password-screen',
-            //customLang: customLangParams,
-            containerID: 'ShowResetPassScreen'
-            
-        });
-
-    
     
 }
