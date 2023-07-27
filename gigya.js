f99fac4cd67c90287f2071403ccb381d8fbd9dab
gpassciam <104811739+gpassciam@users.noreply.github.com>
2023-07-28 01:45:59 +0530

    Update gigya.js

--- gigya.js
+++ gigya.js
@@ -1,12 +1,12 @@
 function ShowLoginScreen(lang)
 {
-    
+    alert(lang);
     gigya.accounts.showScreenSet({
             screenSet: 'Localization-China-RegistrationLogin',
             startScreen: 'gigya-login-screen',
             //customLang: customLangParams,
             containerID: 'LoginScreen',
-            lang: 'es'
+            lang: lang
             
         });
 
